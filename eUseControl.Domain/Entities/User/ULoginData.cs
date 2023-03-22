fa7c9aa41c69c5cc480419a6939a843c247768ae
Nadejda Lica <nadejda.lica@isa.utm.md>
2023-03-22 22:24:38 +0200

    eUseControl.Domain
    Co-Authored-By: MarinelaRoman <124778450+MarinelaRoman@users.noreply.github.com>

---
+++ eUseControl.Domain/Entities/User/ULoginData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eUseControl.Domain.Enums;
+
+namespace eUseControl.Domain.Entities.User
+{
+    public class ULoginData
+    {
+        public string Credential { get; set; }
+        public string Password { get; set; }
+        public string LoginIp { get; set; }
+        public DateTime LoginDateTime { get; set; }
+        public URole Level { get; set; }
+    }
+}
---
+++ eUseControl.Domain/Entities/User/URegisterData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using eUseControl.Domain.Enums;
+
+namespace eUseControl.Domain.Entities.User
+{
+    public class URegisterData
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Password { get; set; }
+        public DateTime RegisterDateTime { get; set; }
+        public URole Level { get; set; }
+    }
+}
