69ebaa3ae5abb630c25c7e75fddf6ae1c35a3c5c
Nadejda Lica <nadejda.lica@isa.utm.md>
2023-03-22 22:11:01 +0200

    eUseControl.Helpers

---
+++ eUseControl.Domain/Entities/Product/DbProduct.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using eUseControl.Domain.Enums;
+
+namespace eUseControl.Domain.Entities.Product
+{
+    public class DbProduct
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Prod_Id { get; set; }
+
+        [Required]
+        [Display(Name = "Product Name")]
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "Name shorter than 5 char or longer than 30")]
+        public string Prod_Name { get; set; }
+
+        [Required]
+        [Display(Name = "Product description")]
+        [StringLength(250, MinimumLength = 8, ErrorMessage = "Minim 8, max 250")]
+        public string Prod_Desc { get; set; }
+
+        [Required]
+        [Display(Name = "Product price")]
+        [StringLength(250, MinimumLength = 1, ErrorMessage = "Minim 1, max 250")]
+        public string Prod_Price { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime LastEditTime { get; set; }
+    }
+}
---
+++ eUseControl.Domain/Entities/Product/UProductData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using eUseControl.Domain.Enums;
+
+namespace eUseControl.Domain.Entities.Product
+{
+    public class UProductData
+    {
+        public string Prod_Name { get; set; }
+        public string Prod_Desc { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Prod_Id { get; set; }
+        public string Prod_Price { get; set; }
+        public DateTime LastEditTime { get; set; }
+    }
+}
