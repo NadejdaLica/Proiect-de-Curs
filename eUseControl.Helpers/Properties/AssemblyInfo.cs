844ec2cbdc5f0fcbc8f6a1fe941f579c73b04cf6
Nadejda Lica <nadejda.lica@isa.utm.md>
2023-03-22 22:35:58 +0200

    eUseControl.Helpers
    Co-Authored-By: MarinelaRoman <124778450+MarinelaRoman@users.noreply.github.com>

---
+++ eUseControl.Helpers/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Informațiile generale despre un ansamblu sunt controlate prin intermediul următoarelor elemente
+// set de atribute. Schimbați valorile acestor atribute pentru a modifica informațiile
+// asociate cu un ansamblu.
+[assembly: AssemblyTitle("eUseControl.Helpers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("eUseControl.Helpers")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setarea ComVisible la false face ca tipurile din acest ansamblu să nu fie vizibile.
+// pentru componentele COM.  Dacă aveți nevoie să accesați un tip din acest ansamblu din
+// COM, setați atributul ComVisible la true pentru tipul respectiv.
+[assembly: ComVisible(false)]
+
+// Următorul GUID este pentru ID-ul typelib dacă acest proiect este expus la COM.
+[assembly: Guid("708a9336-b5a3-4824-8cca-9eeb145b74b1")]
+
+// Informațiile privind versiunea unui ansamblu sunt formate din următoarele patru valori:
+//
+// Versiunea majoră
+// Versiunea minoră
+// Numărul de construcție
+// Revizuirea
+//
+// Puteți specifica toate valorile sau puteți specifica implicit numerele de construcție și de revizuire.
+// utilizând "*", după cum se arată mai jos:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ eUseControl.Helpers/Session/GenUniqueData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eUseControl.Helpers.Session
+{
+    public static class GenUniqueData
+    {
+        public static string GenUniqueCookieByCredential(string uC)
+        {
+            return string.Empty;
+        }
+    }
+}
